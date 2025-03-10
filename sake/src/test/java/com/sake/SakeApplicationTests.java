91ba6634ddf7a3855485d05b38d9b49a3ab13ee8
YONGYONGB <aa39421371@gmail.com>
2025-03-10 11:51:11 +0900

    25.3.10

---
+++ sake/src/test/java/com/sake/SakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
