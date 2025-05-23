9757f65651903630665d4334122fde7d404352c5
YONGYONGB <aa39421371@gmail.com>
2025-05-23 18:50:22 +0900

    Create AccountCodeDto.java

---
+++ sake/src/main/java/com/sake/user/account/AccountCodeDto.java
@@ -0,0 +1,83 @@
+package com.sake.user.account;
+
+public class AccountCodeDto {
+
+	
+	private String user_id;
+	// address------
+		private String ua_id;
+		private String zipCode;
+		private String ua_address;
+		private String detailAd;
+		private String reference;
+		private String latitude;
+		private String longitude;
+		private String user_user_id;
+		private Integer ua_rep;
+		
+		
+		
+		
+		public String getUser_id() {
+			return user_id;
+		}
+		public void setUser_id(String user_id) {
+			this.user_id = user_id;
+		}
+		public String getUa_id() {
+			return ua_id;
+		}
+		public void setUa_id(String ua_id) {
+			this.ua_id = ua_id;
+		}
+		public String getZipCode() {
+			return zipCode;
+		}
+		public void setZipCode(String zipCode) {
+			this.zipCode = zipCode;
+		}
+		public String getUa_address() {
+			return ua_address;
+		}
+		public void setUa_address(String ua_address) {
+			this.ua_address = ua_address;
+		}
+		public String getDetailAd() {
+			return detailAd;
+		}
+		public void setDetailAd(String detailAd) {
+			this.detailAd = detailAd;
+		}
+		public String getReference() {
+			return reference;
+		}
+		public void setReference(String reference) {
+			this.reference = reference;
+		}
+		public String getLatitude() {
+			return latitude;
+		}
+		public void setLatitude(String latitude) {
+			this.latitude = latitude;
+		}
+		public String getLongitude() {
+			return longitude;
+		}
+		public void setLongitude(String longitude) {
+			this.longitude = longitude;
+		}
+		public String getUser_user_id() {
+			return user_user_id;
+		}
+		public void setUser_user_id(String user_user_id) {
+			this.user_user_id = user_user_id;
+		}
+		public Integer getUa_rep() {
+			return ua_rep;
+		}
+		public void setUa_rep(Integer ua_rep) {
+			this.ua_rep = ua_rep;
+		}
+		
+			
+}
