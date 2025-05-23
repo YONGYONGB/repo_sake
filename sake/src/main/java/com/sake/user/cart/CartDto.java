03cb50a49adede9be251870049c07cb3717cef7d
YONGYONGB <aa39421371@gmail.com>
2025-05-23 18:52:20 +0900

    Update CartDto.java

--- sake/src/main/java/com/sake/user/cart/CartDto.java
+++ sake/src/main/java/com/sake/user/cart/CartDto.java
@@ -1,87 +1,127 @@
 package com.sake.user.cart;
 
-public class CartDto {
+import com.sake.module.base.BaseDto;
 
-	private String op_id;
-	private Integer op_price;
-	private Integer product_quantity;
-	private Double personal_rating;
-	private String review;
+public class CartDto extends BaseDto{
+
+	private String cart_id;
+	private Integer cart_price;
+	private Integer cart_quantity;
 	private String product_pd_id;
-	private String order_od_id;
-	private String order_user_user_id;
+	private String user_user_id;
+	private String cart_regDate;
 	
 	
 	private String pd_id;
 	private String name;
+	private Integer pd_quantity;
 	
+	private String user_id;
 	
-	
-	
-	
-	
-	
-	
-	public String getOp_id() {
-		return op_id;
+
+	public String getCart_id() {
+		return cart_id;
 	}
-	public void setOp_id(String op_id) {
-		this.op_id = op_id;
+
+
+	public void setCart_id(String cart_id) {
+		this.cart_id = cart_id;
 	}
-	public Integer getProduct_quantity() {
-		return product_quantity;
+
+
+	public Integer getCart_price() {
+		return cart_price;
 	}
-	public void setProduct_quantity(Integer product_quantity) {
-		this.product_quantity = product_quantity;
+
+
+	public void setCart_price(Integer cart_price) {
+		this.cart_price = cart_price;
 	}
-	public Double getPersonal_rating() {
-		return personal_rating;
+
+
+	public Integer getCart_quantity() {
+		return cart_quantity;
 	}
-	public void setPersonal_rating(Double personal_rating) {
-		this.personal_rating = personal_rating;
+
+
+	public void setCart_quantity(Integer cart_quantity) {
+		this.cart_quantity = cart_quantity;
 	}
-	public String getReview() {
-		return review;
-	}
-	public void setReview(String review) {
-		this.review = review;
-	}
+
+
 	public String getProduct_pd_id() {
 		return product_pd_id;
 	}
+
+
 	public void setProduct_pd_id(String product_pd_id) {
 		this.product_pd_id = product_pd_id;
 	}
-	public String getOrder_od_id() {
-		return order_od_id;
+
+
+	public String getUser_user_id() {
+		return user_user_id;
 	}
-	public void setOrder_od_id(String order_od_id) {
-		this.order_od_id = order_od_id;
+
+
+	public void setUser_user_id(String user_user_id) {
+		this.user_user_id = user_user_id;
 	}
-	public String getOrder_user_user_id() {
-		return order_user_user_id;
-	}
-	public void setOrder_user_user_id(String order_user_user_id) {
-		this.order_user_user_id = order_user_user_id;
-	}
-	public Integer getOp_price() {
-		return op_price;
-	}
-	public void setOp_price(Integer op_price) {
-		this.op_price = op_price;
-	}
+
+
 	public String getPd_id() {
 		return pd_id;
 	}
+
+
 	public void setPd_id(String pd_id) {
 		this.pd_id = pd_id;
 	}
+
+
 	public String getName() {
 		return name;
 	}
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+
+	public String getCart_regDate() {
+		return cart_regDate;
+	}
+
+
+	public void setCart_regDate(String cart_regDate) {
+		this.cart_regDate = cart_regDate;
+	}
+
+
+	public Integer getPd_quantity() {
+		return pd_quantity;
+	}
+
+
+	public void setPd_quantity(Integer pd_quantity) {
+		this.pd_quantity = pd_quantity;
+	}
+	
+	
+	
+	
+	
 	
 	
 	
