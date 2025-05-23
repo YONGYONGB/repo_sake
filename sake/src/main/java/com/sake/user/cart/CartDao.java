5aacf5466744ab8d6e1c54a8eeb461fd4b5a10a6
YONGYONGB <aa39421371@gmail.com>
2025-05-23 18:53:45 +0900

    Create CartDao.java

---
+++ sake/src/main/java/com/sake/user/cart/CartDao.java
@@ -0,0 +1,15 @@
+package com.sake.user.cart;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartDao {
+	
+	public List<CartDto> cartList(CartDto dto);
+	
+	public int cartInsert(CartDto dto);
+	
+	public int cartDelete(String cart_id);
+}
