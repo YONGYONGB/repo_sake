18f9de18b93d49060e3b962c4760720d7b057885
YONGYONGB <aa39421371@gmail.com>
2025-05-23 18:48:36 +0900

    Update AccountCodeService.java

--- sake/src/main/java/com/sake/user/account/AccountCodeService.java
+++ sake/src/main/java/com/sake/user/account/AccountCodeService.java
@@ -11,9 +11,13 @@ import com.sake.module.member.MemberDto;
 @Service
 public class AccountCodeService {
 	@Autowired
+	AccountCodeDao dao;
+	
+	@Autowired
 	MemberDao memberDao;
-
+	////////
 	// 계정등록
+	///////
 	public MemberDto accountSetting(String num){
 		return memberDao.accountSetting(num);
 	}
@@ -30,14 +34,23 @@ public class AccountCodeService {
 		return memberDao.withdrawAction(user_id);
 	}
 	
+	///////
 	// 주소
-	public List<MemberDto> addressList(MemberDto dto){
-		return memberDao.addressList(dto);
+	///////
+	public List<AccountCodeDto> addressList(AccountCodeDto dto){
+		return dao.addressList(dto);
 	}
 
-	public int addressInsert(MemberDto dto){
-		return memberDao.addressInsert(dto);
+	public int addressInsert(AccountCodeDto dto){
+		return dao.addressInsert(dto);
+	}
+	public int addressDelete(AccountCodeDto dto){
+		return dao.addressDelete(dto);
 	}
 	
+	public void updateRepresentive(String ua_id, String seq) {
+		dao.resetrep(seq);
+		dao.setrep(seq,ua_id);
+	}
 	
 }
