2c89530e18e64bebeb6898246bf73871f8ea2ae0
YONGYONGB <aa39421371@gmail.com>
2025-05-23 18:53:02 +0900

    Update CartService.java

--- sake/src/main/java/com/sake/user/cart/CartService.java
+++ sake/src/main/java/com/sake/user/cart/CartService.java
@@ -1,8 +1,18 @@
-package com.sake.user.cart;
-
-import org.springframework.stereotype.Service;
-
 @Service
 public class CartService {
 
+	@Autowired
+	CartDao dao;
+	
+	public List<CartDto> cartList(CartDto dto){
+		return dao.cartList(dto);
+	}
+	
+	public int cartInsert(CartDto dto) {
+		return dao.cartInsert(dto);
+	}
+	
+	public int cartDelete(String cart_id) {
+		return dao.cartDelete(cart_id);
+	}
 }
