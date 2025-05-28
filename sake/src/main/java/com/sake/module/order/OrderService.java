96a2b0beddd516018604b1e8ea52d3d330779b65
YONGYONGB <aa39421371@gmail.com>
2025-05-28 10:11:29 +0900

    kyy

--- sake/src/main/java/com/sake/module/order/OrderService.java
+++ sake/src/main/java/com/sake/module/order/OrderService.java
@@ -1,8 +1,30 @@
 package com.sake.module.order;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 
+	@Autowired
+	OrderDao dao;
+	
+	public int Uoinsert(OrderDto dto) {
+		return dao.Uoinsert(dto);
+	}
+	
+	public int Opinsert(String cart_id, String userOrder_od_id) {
+		return dao.Opinsert(cart_id, userOrder_od_id);
+	}
+	
+	public List<String> findUserUoseq(String user_user_id){
+		return dao.findUserUoseq(user_user_id);
+	}
+	public List<OrderDto> findOpList(List<String>odIdList){
+		return dao.findOpList(odIdList);
+	}
+	
 }
