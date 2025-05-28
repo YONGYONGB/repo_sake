d5105e270f87166ff3fde70bec1f94bbfa713b48
YONGYONGB <aa39421371@gmail.com>
2025-05-28 12:11:16 +0900

    facebook 기초만 설정.

--- sake/src/main/java/com/sake/common/config/WebMvcConfigurerImpl.java
+++ sake/src/main/java/com/sake/common/config/WebMvcConfigurerImpl.java
@@ -10,7 +10,7 @@ import com.sake.common.interceptor.CheckLoginSessionInterceptor;
 public class WebMvcConfigurerImpl implements WebMvcConfigurer{
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {       //WebMvcConfigurerImpl 클래스에서 하는 일(인터셉터 등록)은 스프링 MVC 레벨의 요청 전처리를 위한 것
 		registry.addInterceptor(new CheckLoginSessionInterceptor())
 //				.order(1)
 				.addPathPatterns("/*/*/*Xdm*", "/*/*/*User*")
